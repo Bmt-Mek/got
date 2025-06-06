1b92db01e68ef918b019f6b86dcb69117a08c5d8
Sayomphu <sayomphu.mek@gmail.com>
2025-06-07 02:56:51 +0700

    Fix: Resolved the issue where router links are not working in footer component.

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [CommonModule, MatIconModule, MatButtonModule],
+  imports: [CommonModule, RouterModule, MatIconModule, MatButtonModule],
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
 })
